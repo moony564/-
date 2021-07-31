014d833c486b67d1483b27e704d284219a684551
moony564 <71317753+moony564@users.noreply.github.com>
2021-07-31 13:22:08 +0900

    Create bot_main.py

---
+++ bot_main.py
@@ -0,0 +1,24 @@
+import discord
+import os
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+
+    print(client.user.name)
+    print("start")
+    game = discord.Game('$도움말 또는 $help')
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+
+@client.event
+async def on_message(message):
+    if message.content == "$help":
+        await message.channel.send("Team Studio's first public work is still being made, so please look forward to it!")
+    if message.content == "$도움말":
+        await message.channel.send("팀 스튜디오의 첫 공개작품 아직은 만들어 지는중이니 기대해 주세요!")
+
+access_token = os.environ["BOT_TOKEN"]
+client.run(access_token)
